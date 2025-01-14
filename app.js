70ea2190fb2aa1599e48a4dc62123f452d794fae
Hasanain Alarady <80710414+Hasanain-Jaafar@users.noreply.github.com>
2025-01-14 23:08:56 +0100

    enprove responsive design and make quote load on page load

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const paragraphOutput = document.getElementById("output");
 const adviceID = document.getElementById("adviceID");
+const fetchNextQuoteBtn = document.getElementById("fetchButton");
 //!
-// function to fetch data from API
-document.getElementById("fetchButton").addEventListener("click", async () => {
+// when the page loaded the default quote that will be shown
+window.onload = async () => {
+  try {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    if (!response.ok) throw new Error("couldn't fetch the data ");
+    const data = await response.json();
+    adviceID.textContent = `#${JSON.stringify(data.slip.id)}`;
+    paragraphOutput.textContent = JSON.stringify(data.slip.advice);
+  } catch (error) {
+    paragraphOutput.textContent = "Error: " + error.message;
+  }
+};
+// function to fetch data from API when Quote Btn clicked 
+fetchNextQuoteBtn.addEventListener("click", async () => {
   try {
     const response = await fetch("https://api.adviceslip.com/advice");
     if (!response.ok) throw new Error("couldn't fetch the data ");
